bb7630eea2851831566694b0f8a0f8a8e39b3e70
hankerman <stalker-2204@mail.ru>
2021-12-26 20:41:00 +0300

    Добавьте файлы проекта.

---
+++ massiv/massiv.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    const int size = 10;
+    int arr[size], arr1[size / 2] = { 1,3,5,7,9 }, arr2[size / 2] = {2,4,6,8,10};
+    for (int i = 0; i < size; i += 2) {
+        arr[i] = arr1[i / 2];
+        arr[i + 1] = arr2[i / 2];
+    }
+
+    for (int i = 0; i < size; i++) {
+        cout << arr[i] << " ";
+    }
+}
